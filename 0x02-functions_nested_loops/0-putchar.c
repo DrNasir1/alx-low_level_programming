4364cafc98c9dd4487e268ac22f5d46e573e2230
DrNasir1 <aremunaseerddeen@gmail.com>
2022-09-14 14:58:29 -0700

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *main - the program starts andends at the main upon successful completion
+ *main-the program starts andends at the main upon successful completion
  *return: the programm returns zero after a successful try
  */
-int main(void)
+int main (void)
 {
 	printf("_putchar\n");
 	return(0);
