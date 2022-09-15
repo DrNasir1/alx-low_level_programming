096dc2f8bbee125028a4c916f4fffe7b9b210786
DrNasir1 <aremunaseerddeen@gmail.com>
2022-09-15 07:41:03 -0700

    ok

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * _isupper - check upper
+ *
+ *   * @c : parameter ofvd function
+ *
+ *    * Return: return per instruction
+ *
+ *     */
+
+
+
+int _isupper(int c)
+
+{
+
+		if (c >= 'A' && c <= 'Z')
+
+				{
+
+							return (1);
+
+								}
+
+			else
+
+					{
+
+								return (0);
+
+									}
+
+}
