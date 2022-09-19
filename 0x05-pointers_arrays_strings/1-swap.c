a9f6b480ef330e966ec03e9da622fe651d05f965
DrNasir1 <aremunaseerddeen@gmail.com>
2022-09-19 21:52:26 +0100

    swapy

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- *swap_int - swap the value of int a and int b
- *@a - first parameter/int
- *@b - second parameter/int
- *return(void)
+ * swap_int - swapping
+ * @a : parameter a
+ * @b: parameter b
+ * Return: void
  */
+
 void swap_int(int *a, int *b)
-
-
 {
 	int s;
+
 	s = *a;
 	*a = *b;
 	*b = s;
