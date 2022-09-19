da5904112c7e62a796c38eabe4ea40d5b6f90a88
DrNasir1 <aremunaseerddeen@gmail.com>
2022-09-19 22:28:10 +0100

    hew

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - copying
+ * @dest : destination
+ * @src : source
+ * Return: the value
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; *(src + i) != '\0'; i++)
+	{
+	}
+	for (j = 0; j <= i; j++)
+	{
+		dest[j] = *(src + j);
+	}
+
+	return (dest);
+}
