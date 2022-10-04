9143cc637c51ee0a2a2a91c12bfb2f3b152c5789
DrNasir1 <aremunaseerddeen@gmail.com>
2022-10-04 22:16:01 +0100

    done

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,35 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <string.h>
+#include <ctype.h>
 
 /**
- * main - main func
- * @argc : arg count
- * @argv: arg vector
- * Return: 0 success
+ * main - prints the minimum number of coins
+ * to make change for an amount of money
+ * @argc: n args
+ * @argv: arr args
+ * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
-	int position, total, change, aux;
-	int coins[] = {25, 10, 5, 2, 1};
+	int value, c;
 
-	position = total = change = aux = 0;
+	c = 0;
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	total = atoi(argv[1]);
-
-	if (total < 0)
+	value = atoi(argv[1]);
+	if (value < 0)
 	{
-		printf("0\n");
+		printf("%d\n", 0);
 		return (0);
 	}
-	while (coins[position] != 0)
+	if (value % 25 >= 0)
 	{
-		if (total >= coins[position])
-		{
-
+		c += value / 25;
+		value = value % 25;
+	}
+	if (value % 10 >= 0)
+	{
+		c += value / 10;
+		value = value % 10;
+	}
+	if (value % 5 >= 0)
+	{
+		c += value / 5;
+		value = value % 5;
+	}
+	if (value % 2 >= 0)
+	{
+		c += value / 2;
+		value = value % 2;
+	}
+	if (value % 1 >= 0)
+	{
+		c += value / 1;
+	}
+	printf("%d\n", c);
+	return (0);
+}
