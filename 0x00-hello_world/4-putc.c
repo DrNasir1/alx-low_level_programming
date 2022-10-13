0f8a4a48b9ebdc29e495b53cd05ba2a522284abd
DrNasir1 <aremunaseerddeen@gmail.com>
2022-10-13 14:55:07 -0700

    try

---
+++ 0x00-hello_world/4-putc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - thi program prints out using puts functions
+ *return: this prprogram returns zero after successful try
+ */
+
+int main()
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
